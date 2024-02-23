bba1c6c87a486b82f182332c7ad7d0b1e24d9265
ggam-nyang <v44ads@naver.com>
2024-02-23 23:47:17 +0900

    algo night

---
+++ programmers/176963.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+
+
+    vector<int> answer;
+    return answer;
+}
